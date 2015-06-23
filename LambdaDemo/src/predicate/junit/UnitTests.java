7382b65a456b59eedff47a1686f0d76d44b6bdde
Steve Beckle <srbeckle@users.noreply.github.com>
2015-06-23 16:54:33 -0500

    Add over30shouldBe420

--- LambdaDemo/src/predicate/junit/UnitTests.java
+++ LambdaDemo/src/predicate/junit/UnitTests.java
@@ -1,7 +1,31 @@
 package predicate.junit;
 
+import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
-public class UnitTests {
+import org.junit.Test;
+
+import predicate.PredicateDemo;
+
+class Helper
+{
+	static Predicate<Integer> isOver30 = (x) -> x > 30;
+	static Predicate<Integer> isOdd = (x) -> x % 2 == 1;
+	static Predicate<Integer> isLarge = (x) -> x > 100;
+	static Predicate<Integer> isSquare = (x) -> Math.sqrt(x) * Math.sqrt(x) == x;
+}
+
+public class UnitTests 
+{
+	List<Integer> scores = Arrays.asList(40, 45, 7, 105, 36, 144, 50, 6, 8, 9);
+	
+	@Test
+	public void over30shouldBe420()
+	{
+		assertTrue("Sum of over 30 should be 420", PredicateDemo.sumAll(scores, Helper.isOver30) == 420);
+	}
 
 }
