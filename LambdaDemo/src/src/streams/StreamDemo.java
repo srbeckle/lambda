4747364abdd653b04642cce8435543c749fb56d8
Steve Beckle <srbeckle@users.noreply.github.com>
2015-06-24 17:12:34 -0500

    added anyMatch

--- LambdaDemo/src/src/streams/StreamDemo.java
+++ LambdaDemo/src/src/streams/StreamDemo.java
@@ -8,6 +8,7 @@ public class StreamDemo {
 	public static void main(String[] args) {
 		List<Integer> scores = Arrays.asList(56, 68, 54, 98, 51, 30, 88, 44);
 		System.out.println(scores.stream().filter((x)-> x > 60).findFirst());
+		System.out.println(scores.stream().anyMatch((x)->x > 200));
 
 	}
 
