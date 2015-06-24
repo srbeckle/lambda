6baf525481b9779bb26d82004653ab4491dac98b
Steve Beckle <srbeckle@users.noreply.github.com>
2015-06-24 16:48:42 -0500

    added code to main

--- LambdaDemo/src/src/streams/StreamDemo.java
+++ LambdaDemo/src/src/streams/StreamDemo.java
@@ -1,9 +1,13 @@
 package src.streams;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StreamDemo {
 
 	public static void main(String[] args) {
-		
+		List<Integer> scores = Arrays.asList(56, 68, 54, 98, 51, 30, 88, 44);
+		System.out.println(scores.stream().filter((x)-> x > 60).findFirst());
 
 	}
 
